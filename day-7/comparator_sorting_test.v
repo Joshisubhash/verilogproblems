32c5af71121b4583f0f9794c5dba51e221fce4f0
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-06-20 23:45:05 +0530

    Update comparator_sorting_test.v

--- day-7/comparator_sorting_test.v
+++ day-7/comparator_sorting_test.v
@@ -1,1 +1,33 @@
 
+module sorting_test();
+
+wire [3:0]x,y,z,w;
+ reg [3:0]a,b,c,d;
+
+
+sorting test(
+
+.a(a),
+.b(b),
+.c(c),
+.d(d),
+.x(x),
+.y(y),
+.z(z),
+.w(w)
+);
+
+
+initial begin
+
+   a = 4'b1011;
+   b = 4'b0011; 
+   c = 4'b0111; 
+   d = 4'b0001;
+  #10;
+ 
+ 
+
+end
+
+endmodule
