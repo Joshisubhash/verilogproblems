a16cb7e21d5a458aa322c289b9e5be49b7adc832
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-06-23 23:44:36 +0530

    Update encoder_test.v

--- day-10/encoder_test.v
+++ day-10/encoder_test.v
@@ -1,1 +1,20 @@
 
+module encoder_test();
+reg [1:0] in;
+wire [3:0] out;
+encoder test (.in(in) , .out(out));
+ initial begin 
+ 
+ in = 2'b00;#10;
+ $display("%b %b ",in,out);
+ in = 2'b01;#10;
+ $display("%b %b ",in,out);
+ in = 2'b10;#10;
+ $display("%b %b ",in,out);
+ in = 2'b11;#10;
+ $display("%b %b ",in,out);
+ $finish;
+ 
+ end
+endmodule
+
