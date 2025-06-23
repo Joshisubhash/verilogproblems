5337f6e1d4f443e9a47b6496df3fc491f522e05d
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-06-24 00:57:36 +0530

    Update encoder_n_decoder_test.v

--- day-10/encoder_n_decoder_test.v
+++ day-10/encoder_n_decoder_test.v
@@ -18,3 +18,66 @@ decoder test (.in(in) , .out(out));
  end
 endmodule
 
+//for 4:16 decoder using 2:4 decoder
+module decoder_test();
+reg [3:0] in;
+wire [15:0] out;
+reg e;
+
+main test (.out(out),.in(in),.e(e) );
+ 
+ initial begin 
+ e = 1;
+ 
+  in = 4'b0000; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0001;  #10;
+   $display("%b %b ",in,out);
+  in = 4'b0010; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0011; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0100; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0101; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0110; #10;
+   $display("%b %b ",in,out);
+  in = 4'b0111; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1000; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1001; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1010; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1011; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1100; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1101; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1110; #10;
+   $display("%b %b ",in,out);
+  in = 4'b1111; #10; 
+   $display("%b %b ",in,out);
+ 
+ 
+ 
+ 
+  e = 0;
+ in = 2'b00;#10;
+ $display("%b %b ",in,out);
+ in = 2'b01;#10;
+ $display("%b %b ",in,out);
+ in = 2'b10;#10;
+ $display("%b %b ",in,out);
+ in = 2'b11;#10;
+ $display("%b %b ",in,out);
+ $finish;
+ 
+ end
+
+
+endmodule
+
