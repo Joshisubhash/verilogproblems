2116081a574d9ef5c45626f396780c1ad4fb6d73
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-06-25 19:24:28 +0530

    Update alu_test.v

--- day-12/alu_test.v
+++ day-12/alu_test.v
@@ -1,1 +1,33 @@
 
+module alu_test();
+
+reg [3:0]a,b;
+reg [2:0]opcode;
+wire [3:0]out;
+
+
+alu test (
+.a(a),
+.b(b),
+.opcode(opcode),
+.out(out)
+);
+
+initial begin
+
+a = 4'b1010;
+b = 4'b0101;#10;
+
+opcode = 3'b000;#10;
+opcode = 3'b001;#10;
+opcode = 3'b010;#10;
+opcode = 3'b011;#10;
+opcode = 3'b100;#10;
+opcode = 3'b101;#10;
+opcode = 3'b110;#10;
+opcode = 3'b111;#10;
+$finish;
+
+end
+
+endmodule
