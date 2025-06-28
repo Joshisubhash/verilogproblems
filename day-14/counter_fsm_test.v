1e1fa117d221b30db70b098e2a8e0386ee073797
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-06-28 22:32:23 +0530

    Update counter_fsm_test.v

--- day-14/counter_fsm_test.v
+++ day-14/counter_fsm_test.v
@@ -1,1 +1,30 @@
+module counter_fsm_test();
 
+ 
+reg clk;
+reg rst;
+reg start;
+wire [3:0]out;
+
+
+counter_fsm test(
+.clk(clk),
+.rst(rst),
+.out(out),
+.start(start)
+
+);
+
+initial clk = 0;
+always #5 clk = ~clk;
+
+initial begin
+start =1;#10;
+start=0;
+rst = 0;
+ 
+
+
+
+end
+ endmodule
