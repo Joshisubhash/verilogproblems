e7a6ed08cd0ce1cf896f669f9e32f5a824d5ac8c
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-07-03 18:04:58 +0530

    Update moore.v

--- day-19/moore.v
+++ day-19/moore.v
@@ -1,1 +1,142 @@
-
+module moore_model_nonoverlapping(out,rst,clk,in);
+ 
+ input wire clk,rst,in;
+ output reg out;
+ 
+ reg [4:0]state;
+ 
+ always@(posedge clk)begin
+ if(rst)begin
+ state<=0;
+ out<=0;
+ end
+ else begin
+ case(state)
+ 0 : begin
+     if(in == 1)begin
+     out<=0;
+     state<=1;
+     end
+     else begin
+     state<=0;
+     out<=0;
+     end
+     end
+ 1: begin
+    if(in == 1)begin
+    out<=0;
+    state<=2;
+    end
+    else begin
+    state<=0;
+    out<=0;
+    end
+    end
+ 2 : begin
+     if(in == 0)begin
+     out<=0;
+     state<=3;
+     end
+     else begin
+     state<=1;
+     out<=0;
+     end
+     end
+ 3: begin
+    if(in==1)begin
+    out<=0;
+    state<=4;
+    end
+    else begin
+    state<=0;
+    out<=0;
+    end
+    end
+ 4: begin
+    if(in==1)begin
+    out<=1;
+    state<=0;
+    end
+    else begin
+    out<=0;
+    state<=0;
+    end
+    end
+ endcase
+ 
+ end
+ end
+ endmodule 
+ 
+ 
+ 
+ 
+ module moore_overlapiing(out,rst,clk,in);
+ 
+ input wire in,clk,rst;
+ output reg out;
+ 
+ reg [4:0]state;
+ 
+ always@(posedge clk)begin
+ if(rst)begin
+ state<=0;
+ out<=0;
+ end
+ else begin
+ case(state)
+ 0 : begin
+     if(in == 1)begin
+     out<=0;
+     state<=1;
+     end
+     else begin
+     state<=0;
+     out<=0;
+     end
+     end
+ 1: begin
+    if(in == 1)begin
+    out<=0;
+    state<=2;
+    end
+    else begin
+    state<=0;
+    out<=0;
+    end
+    end
+ 2 : begin
+     if(in == 0)begin
+     out<=0;
+     state<=3;
+     end
+     else begin
+     state<=2;
+     out<=0;
+     end
+     end
+ 3: begin
+    if(in==1)begin
+    out<=0;
+    state<=4;
+    end
+    else begin
+    state<=0;
+    out<=0;
+    end
+    end
+ 4: begin
+    if(in==1)begin
+    out<=1;
+    state<=2;
+    end
+    else begin
+    state <= 0;
+    out <=0;
+    end
+    end
+    endcase
+  end
+ end
+ 
+ endmodule
