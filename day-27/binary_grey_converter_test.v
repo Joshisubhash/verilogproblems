fc5ea3120f1f4edb5b1b4e9f79637fc5fbff8fd0
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-07-12 18:29:59 +0530

    Update binary_grey_converter_test.v

--- day-27/binary_grey_converter_test.v
+++ day-27/binary_grey_converter_test.v
@@ -1,1 +1,51 @@
+//test bench for binary to grey
+module b2g_test();
+
+reg clk,rst; 
+wire [3:0]grey;
+
+b2g test(
+.clk(clk),
+  .rst(rst), 
+.grey(grey)
+
+);
+
+initial clk=0;
+always #5 clk= ~clk;
+
+initial begin
+
+rst = 1;#5;
+rst=0;
+
+end
+
+
+endmodule
+
+
+//test bench for real time grey to binary converter
+module g2b_test();
+
+reg clk,rst; 
+wire [3:0]grey_out;
+
+g2b test(
+.clk(clk),
+  .rst(rst), 
+.grey_out(grey_out)
+
+);
+
+initial clk=0;
+always #5 clk= ~clk;
+
+initial begin
+
+rst = 1;#5;
+rst=0;
+
+end
+endmodule
 
