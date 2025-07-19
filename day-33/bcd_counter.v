4f468f86ed9028b02903afeac2ee0e3754cb8bcc
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-07-19 22:46:19 +0530

    Update bcd_counter.v

--- day-33/bcd_counter.v
+++ day-33/bcd_counter.v
@@ -11,7 +11,7 @@ wire [4:0]mid;
 assign mid = a + b + cin;
 assign cout = mid > 9 ? 1 : 0;
 assign out = mid > 9 ? mid[3:0] + 6 : mid[3:0];
-// assign cout = mid > 9 ? 1 : 0;
+ 
 
 always@(*) begin
  $display("%b + %b + %b = %b(mid) , %b(cout) , %b(out)",a,b,cin,mid,cout,out);
