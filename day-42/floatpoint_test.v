5a79debb1cab85af159a18e7261f57f6eeb3e1a8
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-07-26 23:17:28 +0530

    Update floatpoint_test.v

--- day-42/floatpoint_test.v
+++ day-42/floatpoint_test.v
@@ -1,1 +1,44 @@
 
+
+ module fp_test();
+ 
+ reg [9:0]a,b;
+ reg clk,rst;
+ wire [9:0]out;
+ 
+ 
+ 
+ fp test(.a(a),.b(b),.clk(clk),.rst(rst),.out(out));
+ 
+ initial clk=0;
+ always #5 clk = ~clk;
+ 
+ initial begin
+ rst = 1;
+    a = 10'b0100101100; b = 10'b0100011000; #30;  
+    rst=0;
+    #100;
+    
+    rst=1;
+    a = 10'b1100101100; b = 10'b0100101100; #30;  
+    rst=0;
+    #100;
+    
+    rst=1;
+    a = 10'b0100110000; b = 10'b0100010000; #30;  
+    rst=0;
+    #100;
+    
+    rst=1;
+    a = 10'b1100110000; b = 10'b0100010000; #30;  
+    rst=0;
+    #100;
+    
+    rst=1;
+    a = 10'b0101101111; b = 10'b0101101111; #30; 
+    rst=0;
+    #100;
+ 
+    $finish;
+    end
+ endmodule
