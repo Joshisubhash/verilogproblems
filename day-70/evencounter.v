3758be548f417eb59b4e8239b5c0e28df0e74b4f
Joshisubhash <121287833+Joshisubhash@users.noreply.github.com>
2025-09-06 17:10:47 +0530

    Create evencounter.v

---
+++ day-70/evencounter.v
@@ -0,0 +1,25 @@
+
+module even_counter(out,rst,clk);
+
+input wire clk,rst;
+output reg [3:0]out;
+
+reg [3:0]count;
+always@(posedge clk)begin
+if(rst)begin
+out <= 0;
+count <= 0;
+
+end else begin
+count <= count + 1'b1;
+//if(count % 2 != 0)begin
+//out <= count;
+//end 
+//if(count[0] != 1)begin
+//out <= count;
+//end
+ count <= count + 2'd2;
+ out <= count;
+end
+ end
+endmodule
